a531ae564ee8972cb3c07eeb1a52fdde400dbe5f
ZALOFARG <andre.work.go@gmail.com>
2025-04-15 11:37:52 -0500

    feat: manage potential errors

--- exercise.py
+++ exercise.py
@@ -24,37 +24,47 @@ def create_report(file):
         none
     """
 
-    with open(file, newline='', encoding='utf-8') as csvfile:
-        number_credit = 0 # iniciamos las variables globales
-        number_debit = 0
-        sum_credit = 0
-        sum_debit = 0
-        max_value = 0
-        id_max = None
-        lector = csv.reader(csvfile) # creamos un objeto reader del modulo
+    try:
+        with open(file, newline='', encoding='utf-8') as csvfile:
+            number_credit = 0 # iniciamos las variables globales
+            number_debit = 0
+            sum_credit = 0
+            sum_debit = 0
+            max_value = 0
+            id_max = None
+            lector = csv.reader(csvfile) # creamos un objeto reader del modulo
 
-        next(lector)
+            next(lector)
 
-        for fila in lector:
-            id_actual = fila[0]
-            monto_actual = float(fila[2])
+            for fila in lector:
+                id_actual = fila[0]
 
-            if monto_actual > max_value: # if que determina el max_value por cada iteracion
-                max_value = monto_actual
-                id_max = id_actual
+                try:
+                    monto_actual = float(fila[2])
+                except ValueError:
+                    print(f"Error: el monto {fila[2]} en la transaccion {id_actual} no es valido. Registro ignorado")
+                    continue
 
-            if fila[1] == "Débito": # if que cuenta el numero de operaciones y monto de cada tipo
-                number_debit += 1
-                sum_debit += float(fila[2])
-            elif fila[1] == "Crédito":
-                number_credit += 1
-                sum_credit += float(fila[2])
+                if monto_actual > max_value: # if que determina el max_value por cada iteracion
+                    max_value = monto_actual
+                    id_max = id_actual
 
-        # Generacion del reporte con lo requerido
-        print("Reporte de Transacciones\n-------------------------------------")
-        print(f"Balance Final: {round(sum_credit - sum_debit, 2)}")
-        print(f"Transacción de Mayor Monto: ID {id_max} - {max_value}")
-        print(f"Conteo de Transacciones: Crédito: {number_credit} Débito: {number_debit}")
+                if fila[1] == "Débito": # if que cuenta el numero de operaciones y monto de cada tipo
+                    number_debit += 1
+                    sum_debit += float(fila[2])
+                elif fila[1] == "Crédito":
+                    number_credit += 1
+                    sum_credit += float(fila[2])
+
+            # Generacion del reporte con lo requerido
+            print("Reporte de Transacciones\n-------------------------------------")
+            print(f"Balance Final: {round(sum_credit - sum_debit, 2)}")
+            print(f"Transacción de Mayor Monto: ID {id_max} - {max_value}")
+            print(f"Conteo de Transacciones: Crédito: {number_credit} Débito: {number_debit}")
+
+    except FileNotFoundError:
+        print("Archivo no encontrado")
+       
 
 if __name__ == '__main__':
     create_report('data.csv')
